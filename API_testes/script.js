8568fe19695e471a14e7c70bc8a85ef6937dad3f
leandro Santana <leandro.stn.dev@gmail.com>
2024-09-04 18:08:14 -0300

    Estudos API

--- API_testes/script.js
+++ API_testes/script.js
@@ -12,19 +12,24 @@ function cadastrar(){
                 'Content-Type': 'application/json'
             },
             method:"POST",
-            body: JSON.stringify({a: 1, b: 2})
+            body: JSON.stringify({
+                nome: iNome.value,
+                email: iEmail.value,
+                senha: iSenha.value,
+                telefone: iTel.value})
         })
         .then(function(res){console.log(res)})
         .catch(function(res){console.log(res)})
 };
-
+function linpar(){
+    iNome.value = "";
+    iEmail.value = "";
+    iSenha.value = "";
+    iTel.value = "";
+}
 formulario.addEventListener('submit', function(event){
     event.preventDefault();
     
-    const dados = {
-        nome: iNome.value,
-        email: iEmail.value,
-        senha: iSenha.value,
-        telefone: iTel.value
-    }
+    cadastrar();
+    linpar();
 });
