ae0fdfc7319d6f063c5598e3175f6620cbccc063
leandro Santana <leandro.edv@hotmail.com>
2025-02-09 15:55:57 -0300

    feat: swagger documentation

--- real-estate/src/main/java/com/santana/real_estate/controller/EstateController.java
+++ real-estate/src/main/java/com/santana/real_estate/controller/EstateController.java
@@ -4,6 +4,7 @@ import com.santana.real_estate.domain.estatedomain.Estate;
 import com.santana.real_estate.dto.estatedto.EstatePostRequestBody;
 import com.santana.real_estate.dto.estatedto.EstatePutRequestBody;
 import com.santana.real_estate.service.estateservice.EstateService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,17 +20,20 @@ public class EstateController {
 
     private final EstateService estateService;
 
+    @Operation(summary = "List all Estate paginated", tags = "estate-list")
     @GetMapping
     public ResponseEntity<Page<Estate>> listAll(Pageable pageable) {
         return new ResponseEntity<>(estateService.findAll(pageable), HttpStatus.OK);
     }
 
-    @GetMapping("/{category}")
+    @Operation(summary = "List all Estate paginated by category",tags = "estate-list")
+    @GetMapping(path = "find/{category}")
     public ResponseEntity<Page<Estate>> findByCategory(@PathVariable String category, Pageable pageable) {
         return new ResponseEntity<>(estateService.findByCategory(category, pageable), HttpStatus.OK);
     }
 
-    @GetMapping("/{category}/{transactionType}")
+    @Operation(summary = "List all Estate paginated by category and transaction type", tags = "estate-list")
+    @GetMapping(path = "find/{category}/{transactionType}")
     public ResponseEntity<Page<Estate>> findByCategoryAndTransactionType(@PathVariable String category,
                                                                          @PathVariable String transactionType,
                                                                          Pageable pageable) {
@@ -37,17 +41,20 @@ public class EstateController {
                 .findByCategoryAndTransactionType(category, transactionType, pageable), HttpStatus.OK);
     }
 
+    @Operation(summary = "Save Estate")
     @PostMapping
     public ResponseEntity<Estate> save(@RequestBody EstatePostRequestBody estatePostRequestBody) {
         return new ResponseEntity<>(estateService.save(estatePostRequestBody), HttpStatus.CREATED);
     }
 
-    @DeleteMapping("{id}")
+    @Operation(summary = "Delete Estate")
+    @DeleteMapping(path = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable long id) {
         estateService.delete(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
+    @Operation(summary = "Update Estate")
     @PutMapping
     public ResponseEntity<Void> replace(@RequestBody EstatePutRequestBody estatePutRequestBody) {
         estateService.replace(estatePutRequestBody);
